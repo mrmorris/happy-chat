a0521179b855d738074e8c751ce85dc06aa89471
Ryan Morris <mr.morris@gmail.com>
2019-11-06 16:22:44 -0500

    Chat token

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = WebSocket.Server;
 const path = require('path');
+const url = require('url');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+const CHAT_TOKEN = process.env.CHAT_TOKEN;
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
@@ -14,7 +16,16 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
+	const params = url.parse(req.url, true).query;
+
+	if (CHAT_TOKEN && (!params.token || params.token != CHAT_TOKEN)) {
+		console.log('Client connection refused');
+		ws.send('unauthorized');
+		ws.close();
+		return;
+	}
+
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 
